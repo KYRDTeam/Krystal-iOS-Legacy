d9e841f01f1f087538cfebfd5dd47085e7083bf1
thangpham26394 <phamducthang26394@gmail.com>
2021-09-29 14:56:59 +0700

    chore(CustomTokenTableViewCell): refactor code, add tick icon
    RE #KRYS-1637

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Settings/CustomTokenList/Cells/CustomTokenTableViewCell.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Settings/CustomTokenList/Cells/CustomTokenTableViewCell.swift
@@ -14,28 +14,29 @@ struct CustomTokenCellViewModel {
 }
 
 class CustomTokenTableViewCell: SwipeTableViewCell {
-  static let kCellID: String = "CustomTokenTableViewCell"
-  static let kCellHeight: CGFloat = 60
-  
-  @IBOutlet weak var imageIcon: UIImageView!
-  @IBOutlet weak var tokenNameLabel: UILabel!
-  @IBOutlet weak var balanceLabel: UILabel!
-  @IBOutlet weak var statusSwitch: UISwitch!
-  
-  var viewModel: CustomTokenCellViewModel?
-  
-  
-  func updateCell(_ viewModel: CustomTokenCellViewModel) {
-    self.viewModel = viewModel
-    self.imageIcon.setSymbolImage(symbol: viewModel.token.symbol, size: CGSize(width: 17, height: 17))
-    self.tokenNameLabel.text = viewModel.token.symbol.uppercased()
-    self.balanceLabel.text = viewModel.balance
-    self.statusSwitch.isOn = KNSupportedTokenStorage.shared.getTokenActiveStatus(viewModel.token)
-  }
-  
-  @IBAction func switchChangedValue(_ sender: UISwitch) {
-    if let notNil = self.viewModel {
-      KNSupportedTokenStorage.shared.setTokenActiveStatus(token: notNil.token, status: sender.isOn)
+    static let kCellID: String = "CustomTokenTableViewCell"
+    static let kCellHeight: CGFloat = 60
+    @IBOutlet weak var imageIcon: UIImageView!
+    @IBOutlet weak var tokenNameLabel: UILabel!
+    @IBOutlet weak var balanceLabel: UILabel!
+    @IBOutlet weak var statusSwitch: UISwitch!
+    @IBOutlet weak var tickIcon: UIImageView!
+    var viewModel: CustomTokenCellViewModel?
+    var onUpdateActiveStatus: (() -> Void)?
+
+    func updateCell(_ viewModel: CustomTokenCellViewModel) {
+        self.viewModel = viewModel
+        self.imageIcon.setSymbolImage(symbol: viewModel.token.symbol, size: CGSize(width: 17, height: 17))
+        self.tokenNameLabel.text = viewModel.token.symbol.uppercased()
+        self.balanceLabel.text = viewModel.balance
+        self.statusSwitch.isOn = KNSupportedTokenStorage.shared.getTokenActiveStatus(viewModel.token)
+        self.tickIcon.isHidden = viewModel.token.getVol(.usd) < 100000
     }
-  }
+
+    @IBAction func switchChangedValue(_ sender: UISwitch) {
+        if let notNil = self.viewModel, let onUpdateActiveStatus = onUpdateActiveStatus {
+          KNSupportedTokenStorage.shared.setTokenActiveStatus(token: notNil.token, status: sender.isOn)
+          onUpdateActiveStatus()
+        }
+    }
 }
