32bbab65ecb7fa407429cd590a90dbbaf546c4aa
thangpham26394 <phamducthang26394@gmail.com>
2021-09-29 14:59:23 +0700

    chore(CustomTokenListViewController): add search action, select all action and display high volumn icon for tokens
    RE #KRYS-1637

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Settings/CustomTokenList/CustomTokenListViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Settings/CustomTokenList/CustomTokenListViewController.swift
@@ -11,14 +11,32 @@ import BigInt
 
 class CustomTokenListViewModel {
   var dataSource: [CustomTokenCellViewModel] = []
-  
+  var currentActiveStatus: Bool {
+    return KNSupportedTokenStorage.shared.activeStatus()
+  }
   func reloadData() {
-    self.dataSource = KNSupportedTokenStorage.shared.getFullCustomToken().map({ (token) -> CustomTokenCellViewModel in
+      self.dataSource = KNSupportedTokenStorage.shared.manageToken.map({ (token) -> CustomTokenCellViewModel in
+        let balance = BalanceStorage.shared.balanceForAddress(token.address)
+        let balanceBigInt = BigInt(balance?.balance ?? "0") ?? BigInt(0)
+        let viewModel = CustomTokenCellViewModel(token: token, balance: balanceBigInt.string(decimals: token.decimals, minFractionDigits: 0, maxFractionDigits: 6))
+        return viewModel
+      })
+  }
+  
+  func filterData(key: String) {
+    let filterTokens = KNSupportedTokenStorage.shared.manageToken.filter({ return ($0.symbol + " " + $0.name).lowercased().contains(key.lowercased()) })
+    
+    
+    self.dataSource = filterTokens.map({ (token) -> CustomTokenCellViewModel in
       let balance = BalanceStorage.shared.balanceForAddress(token.address)
       let balanceBigInt = BigInt(balance?.balance ?? "0") ?? BigInt(0)
       let viewModel = CustomTokenCellViewModel(token: token, balance: balanceBigInt.string(decimals: token.decimals, minFractionDigits: 0, maxFractionDigits: 6))
       return viewModel
     })
+  }
+
+  func changeAllTokensActiveStatus() {
+    KNSupportedTokenStorage.shared.changeAllTokensActiveStatus(isActive: !currentActiveStatus)
   }
 }
 
@@ -33,15 +51,16 @@ protocol CustomTokenListViewControllerDelegate: class {
 }
 
 class CustomTokenListViewController: KNBaseViewController {
-  
   @IBOutlet weak var tokenTableView: UITableView!
   let viewModel = CustomTokenListViewModel()
   weak var delegate: CustomTokenListViewControllerDelegate?
   @IBOutlet weak var emptyView: UIView!
-  
+  @IBOutlet weak var searchTextField: UITextField!
+  @IBOutlet weak var selectButton: UIButton!
+  @IBOutlet weak var searchView: UIView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     let nib = UINib(nibName: CustomTokenTableViewCell.className, bundle: nil)
     self.tokenTableView.register(
       nib,
@@ -59,16 +78,40 @@ class CustomTokenListViewController: KNBaseViewController {
     self.viewModel.reloadData()
     self.emptyView.isHidden = !self.viewModel.dataSource.isEmpty
     self.tokenTableView.reloadData()
+    self.searchView.rounded(radius: self.searchView.frame.size.height/2)
+    updateSelectAllButtonTitle()
+    self.searchTextField.delegate = self
+    self.searchTextField.setPlaceholder(text: "Search".toBeLocalised(), color: UIColor(named: "normalTextColor")!)
   }
   
+  func updateSelectAllButtonTitle() {
+    self.selectButton.setTitle(self.viewModel.currentActiveStatus ? "DESELECT ALL".toBeLocalised() : "SELECT ALL".toBeLocalised(), for: .normal)
+  }
+  
+  func refreshDataSource() {
+    guard let key = self.searchTextField.text else { return }
+    if key.isEmpty {
+      self.viewModel.reloadData()
+    } else {
+      self.viewModel.filterData(key: key)
+    }
+  }
+  
+  @IBAction func selectAllButtonTapped(_ sender: Any) {
+    self.viewModel.changeAllTokensActiveStatus()
+    refreshDataSource()
+    tokenTableView.reloadData()
+    updateSelectAllButtonTitle()
+  }
+    
   @IBAction func backButtonTapped(_ sender: UIButton) {
     self.navigationController?.popViewController(animated: true)
   }
-  
+
   @IBAction func addTokenButtonTapped(_ sender: UIButton) {
     self.delegate?.customTokenListViewController(self, run: .add)
   }
-  
+
   func coordinatorDidUpdateTokenList() {
     guard self.isViewLoaded else {
       return
@@ -89,6 +132,9 @@ extension CustomTokenListViewController: UITableViewDataSource {
     ) as! CustomTokenTableViewCell
 
     cell.updateCell(self.viewModel.dataSource[indexPath.row])
+    cell.onUpdateActiveStatus = {
+      self.updateSelectAllButtonTitle()
+    }
     cell.delegate = self
     return cell
   }
@@ -130,4 +176,11 @@ extension CustomTokenListViewController: SwipeTableViewCellDelegate {
 
     return options
   }
+}
+
+extension CustomTokenListViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+      refreshDataSource()
+      tokenTableView.reloadData()
+    }
 }
