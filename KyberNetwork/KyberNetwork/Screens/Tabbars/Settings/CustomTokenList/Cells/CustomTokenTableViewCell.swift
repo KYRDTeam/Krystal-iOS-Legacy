8c0eefe01ab1469870507cc2655d6d4b05b99219
thangpham26394 <phamducthang26394@gmail.com>
2021-10-01 15:11:06 +0700

    chore(CustomTokenTableViewCell): add new tick icon
    RE #KRYS-1637

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Settings/CustomTokenList/Cells/CustomTokenTableViewCell.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Settings/CustomTokenList/Cells/CustomTokenTableViewCell.swift
@@ -13,6 +13,9 @@ struct CustomTokenCellViewModel {
   let balance: String
 }
 
+/// Value in USD to validate if current token should display blue tick or not
+let hightVolAmount = 100000.0
+
 class CustomTokenTableViewCell: SwipeTableViewCell {
     static let kCellID: String = "CustomTokenTableViewCell"
     static let kCellHeight: CGFloat = 60
@@ -25,18 +28,18 @@ class CustomTokenTableViewCell: SwipeTableViewCell {
     var onUpdateActiveStatus: (() -> Void)?
 
     func updateCell(_ viewModel: CustomTokenCellViewModel) {
-        self.viewModel = viewModel
-        self.imageIcon.setSymbolImage(symbol: viewModel.token.symbol, size: CGSize(width: 17, height: 17))
-        self.tokenNameLabel.text = viewModel.token.symbol.uppercased()
-        self.balanceLabel.text = viewModel.balance
-        self.statusSwitch.isOn = KNSupportedTokenStorage.shared.getTokenActiveStatus(viewModel.token)
-        self.tickIcon.isHidden = viewModel.token.getVol(.usd) < 100000
+      self.viewModel = viewModel
+      self.imageIcon.setSymbolImage(symbol: viewModel.token.symbol, size: CGSize(width: 17, height: 17))
+      self.tokenNameLabel.text = viewModel.token.symbol.uppercased()
+      self.balanceLabel.text = viewModel.balance
+      self.statusSwitch.isOn = KNSupportedTokenStorage.shared.getTokenActiveStatus(viewModel.token)
+      self.tickIcon.isHidden = viewModel.token.getVol(.usd) < hightVolAmount || KNSupportedTokenStorage.shared.getFullCustomToken().contains(viewModel.token)
     }
 
     @IBAction func switchChangedValue(_ sender: UISwitch) {
-        if let notNil = self.viewModel, let onUpdateActiveStatus = onUpdateActiveStatus {
-          KNSupportedTokenStorage.shared.setTokenActiveStatus(token: notNil.token, status: sender.isOn)
-          onUpdateActiveStatus()
-        }
+      if let notNil = self.viewModel, let onUpdateActiveStatus = onUpdateActiveStatus {
+        KNSupportedTokenStorage.shared.setTokenActiveStatus(token: notNil.token, status: sender.isOn)
+        onUpdateActiveStatus()
+      }
     }
 }
