d1cd425f2ad86c272125a9c29a076e5ad05e5816
thangpham26394 <phamducthang26394@gmail.com>
2021-10-20 11:19:12 +0700

    chore(ClaimButtonTableViewCell): refactor cell
    RE #KRYS-1654

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Rewards/Cells/ClaimButtonTableViewCell.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Rewards/Cells/ClaimButtonTableViewCell.swift
@@ -10,13 +10,15 @@ import UIKit
 class ClaimButtonTableViewCell: UITableViewCell {
   static let kCellID: String = "ClaimButtonTableViewCell"
   @IBOutlet weak var bgView: UIView!
-  @IBOutlet weak var claimButton: UILabel!
-  
+  var onClaimButtonTapped: (() -> Void)?
   override func awakeFromNib() {
     super.awakeFromNib()
-   
   }
-  
+
+  @IBAction func onClaimButtonTapped(_ sender: Any) {
+    guard let onClaimButtonTapped = onClaimButtonTapped else { return }
+    onClaimButtonTapped()
+  }
   override func layoutSubviews() {
     bgView.roundWithCustomCorner(corners: [.bottomRight, .bottomLeft], radius: 16)
   }
@@ -26,5 +28,5 @@ class ClaimButtonTableViewCell: UITableViewCell {
 
     // Configure the view for the selected state
   }
-    
+  
 }
