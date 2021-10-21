c42f00f1d37ad908db5f2bae7f13d2f3929e2ed5
thangpham26394 <phamducthang26394@gmail.com>
2021-10-21 09:33:38 +0700

    chore(ClaimButtonTableViewCell): set state for cell
    RE #KRYS-1654

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Rewards/Cells/ClaimButtonTableViewCell.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Rewards/Cells/ClaimButtonTableViewCell.swift
@@ -10,9 +10,16 @@ import UIKit
 class ClaimButtonTableViewCell: UITableViewCell {
   static let kCellID: String = "ClaimButtonTableViewCell"
   @IBOutlet weak var bgView: UIView!
+  @IBOutlet weak var claimButton: UIButton!
   var onClaimButtonTapped: (() -> Void)?
+
   override func awakeFromNib() {
     super.awakeFromNib()
+  }
+
+  func setClaimButtonState(isEnabled: Bool) {
+    self.claimButton.isEnabled = isEnabled
+    self.claimButton.backgroundColor = isEnabled ? UIColor(named: "buttonBackgroundColor")! : UIColor(named: "buttonBackgroundColor")!.withAlphaComponent(0.5)
   }
 
   @IBAction func onClaimButtonTapped(_ sender: Any) {
