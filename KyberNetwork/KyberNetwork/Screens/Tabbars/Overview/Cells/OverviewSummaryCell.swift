4836ae035dbab1ced0bf2373e3ce27525c4a6292
thangpham26394 <phamducthang26394@gmail.com>
2021-11-05 14:24:07 +0700

    chore(OverviewSummaryCell): update data for cell
    RE #KRYS-1759

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Overview/Cells/OverviewSummaryCell.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Overview/Cells/OverviewSummaryCell.swift
@@ -7,6 +7,54 @@
 
 import UIKit
 
+class OverviewSummaryCellViewModel {
+  let currency: CurrencyMode
+  let symbol: String
+  let chainName: String
+  let value: Double
+  let percentage: Double
+  
+  var hideBalanceStatus: Bool = true
+  var chainType: ChainType?
+
+  init(dataModel: KNSummaryChainModel, currency: CurrencyMode) {
+    self.currency = currency
+    self.symbol = dataModel.chainName
+    self.chainName = dataModel.chainName
+    self.chainType = dataModel.chainType()
+    self.percentage = dataModel.percentage
+    if let unitValueModel = dataModel.quotes[currency.toString()] {
+      self.value = unitValueModel.value
+    } else {
+      self.value = 0.0
+    }
+  }
+
+  func balanceValue() -> String {
+    guard !self.hideBalanceStatus else {
+      return "********"
+    }
+
+    let currencyFormatter = StringFormatter()
+    return self.currency.symbol() + currencyFormatter.currencyString(value: self.value, decimals: self.currency.decimalNumber())
+  }
+  
+  func chainIconImage() -> UIImage {
+    switch self.chainType {
+    case .eth:
+      return UIImage(named: "chain_eth_icon")!
+    case .bsc:
+      return UIImage(named: "chain_bsc_icon")!
+    case .polygon:
+      return UIImage(named: "chain_polygon_big_icon")!
+    case .avalanche:
+      return UIImage(named: "chain_avax_icon")!
+    default:
+      return UIImage(named: "default_token")!
+    }
+  }
+}
+
 class OverviewSummaryCell: UITableViewCell {
   static let kCellID: String = "OvereviewSummaryCell"
   @IBOutlet weak var chainIcon: UIImageView!
@@ -23,6 +71,13 @@ class OverviewSummaryCell: UITableViewCell {
       super.setSelected(selected, animated: animated)
 
       // Configure the view for the selected state
+  }
+
+  func updateCell(_ viewModel: OverviewSummaryCellViewModel) {
+    self.chainIcon.image = viewModel.chainIconImage()
+    self.chainNameLabel.text = viewModel.chainName
+    self.chainValueLabel.text = viewModel.balanceValue()
+    self.percentLabel.text = StringFormatter.percentString(value: viewModel.percentage)
   }
 
 }
