cefaa8a19dee5751a0f99ad0373b3f23933fd140
thangpham26394 <phamducthang26394@gmail.com>
2021-11-08 09:39:53 +0700

    chore(UIDevice+Kyber): refactor code
    RE #KRYS-1759

--- KyberNetwork/KyberNetwork/Extensions/UIDevice+Kyber.swift
+++ KyberNetwork/KyberNetwork/Extensions/UIDevice+Kyber.swift
@@ -7,6 +7,6 @@ extension UIDevice {
   public static let isIphone5: Bool = UIScreen.main.bounds.size.height == 568.0
   public static let isIphone6: Bool = UIScreen.main.bounds.size.height == 667
   public static let isIphone6Plus: Bool = UIScreen.main.bounds.size.height == 736
-  public static let isIphoneX: Bool = UIScreen.main.bounds.size.height == 812
+  public static let isIphoneXOrLater: Bool = UIScreen.main.bounds.size.height >= 812
 
 }
