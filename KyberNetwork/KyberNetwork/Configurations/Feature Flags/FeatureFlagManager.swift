03a571bd3b7afa2bccb2d78b9eec353245d3d240
thangpham26394 <phamducthang26394@gmail.com>
2022-03-01 15:40:26 +0700

    add(FeatureFlagManager): add util class for config and get flags info
    RE #KRYS-2500, #KRYS-2516

---
+++ KyberNetwork/KyberNetwork/Configurations/Feature Flags/FeatureFlagManager.swift
@@ -0,0 +1,37 @@
+//
+//  FeatureFlagManager.swift
+//  KyberNetwork
+//
+//  Created by Com1 on 28/02/2022.
+//
+
+import LaunchDarkly
+
+let mobileKey = "mob-23b6e6df-bf90-494b-90e9-85c1d59ab4a2"
+
+public struct FeatureFlagKeys {
+  public static let bifinityIntegration = "bifinity-integration"
+}
+
+class FeatureFlagManager {
+  static let shared = FeatureFlagManager()
+  
+  func configClient(session: KNSession) {
+    let currentAddress = session.wallet.address.description.lowercased()
+
+    var config = LDConfig(mobileKey: mobileKey)
+    config.backgroundFlagPollingInterval = 60
+    let user = LDUser(key: currentAddress)
+    if let client = LDClient.get() {
+      client.identify(user: user)
+    }
+    LDClient.start(config: config, user: user) {
+      KNNotificationUtil.postNotification(for: kUpdateFeatureFlag)
+    }
+  }
+
+  func showFeature(forKey flagKey: String) -> Bool {
+    let client = LDClient.get()!
+    return client.variation(forKey: flagKey, defaultValue: false)
+  }
+}
