5a3853a22b816efd6a3b1772c6f97630e81b639d
thangpham26394 <phamducthang26394@gmail.com>
2022-03-16 13:47:22 +0700

    feat(feature-flag): refactor code
    RE #KRYS-2500

--- KyberNetwork/KyberNetwork/Configurations/Feature Flags/FeatureFlagManager.swift
+++ KyberNetwork/KyberNetwork/Configurations/Feature Flags/FeatureFlagManager.swift
@@ -7,8 +7,6 @@
 
 import LaunchDarkly
 
-let mobileKey = "mob-23b6e6df-bf90-494b-90e9-85c1d59ab4a2"
-
 public struct FeatureFlagKeys {
   public static let bifinityIntegration = "bifinity-integration"
 }
@@ -19,7 +17,7 @@ class FeatureFlagManager {
   func configClient(session: KNSession) {
     let currentAddress = session.wallet.address.description.lowercased()
 
-    var config = LDConfig(mobileKey: mobileKey)
+    var config = LDConfig(mobileKey: KNEnvironment.default.mobileKey)
     config.backgroundFlagPollingInterval = 60
     let user = LDUser(key: currentAddress)
     if let client = LDClient.get() {
