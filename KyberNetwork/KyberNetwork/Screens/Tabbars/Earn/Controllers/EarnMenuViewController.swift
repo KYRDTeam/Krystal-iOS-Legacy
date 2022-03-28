487ec9c9808e58a487a193bc44b854e72c92fb5b
thangpham26394 <phamducthang26394@gmail.com>
2022-03-28 11:37:35 +0700

    chore(EarnMenuViewController): update empty label
    RE #KRYS-2726

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Earn/Controllers/EarnMenuViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Earn/Controllers/EarnMenuViewController.swift
@@ -67,7 +67,7 @@ class EarnMenuViewController: KNBaseViewController {
       .foregroundColor: UIColor(named: "buttonBackgroundColor")
     ], range: NSRange(location: 39, length: 13))
     self.mainInfoTitle.attributedText = attributedString
-    self.noDataLabel.text = (KNGeneralProvider.shared.currentChain == .avalanche || KNGeneralProvider.shared.currentChain == .fantom) ? "Coming soon" : "No data"
+    self.noDataLabel.text = (KNGeneralProvider.shared.currentChain == .eth || KNGeneralProvider.shared.currentChain == .bsc || KNGeneralProvider.shared.currentChain == .polygon) ? "No data" : "Coming soon"
   }
 
   override func viewWillAppear(_ animated: Bool) {
