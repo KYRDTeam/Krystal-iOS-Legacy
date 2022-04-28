30d6d49deabc4b9de73e202cb6d9b554cf96046b
Nguyen Tung <tung@Nguyens-MacBook-Pro.local>
2022-04-28 17:52:11 +0700

    Hide explore items for solana

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Invest/Controllers/InvestViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Invest/Controllers/InvestViewController.swift
@@ -36,22 +36,14 @@ class InvestViewController: KNBaseViewController {
     
     self.setupCollectionView()
     self.bindViewModel()
-    self.observeFeatureFlagChanged()
+    self.viewModel.onViewLoaded()
   }
   
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
     
     self.updateUISwitchChain()
-    self.configFeatureFlag()
-  }
-  
-  deinit {
-    NotificationCenter.default.removeObserver(
-      self,
-      name: Notification.Name(kUpdateFeatureFlag),
-      object: nil
-    )
+    self.viewModel.reloadMenuItems()
   }
   
   func setupCollectionView() {
@@ -82,30 +74,6 @@ class InvestViewController: KNBaseViewController {
     collectionView.reloadSections(.init(arrayLiteral: index))
   }
   
-  fileprivate func observeFeatureFlagChanged() {
-    NotificationCenter.default.addObserver(
-      self,
-      selector: #selector(configFeatureFlag),
-      name: Notification.Name(kUpdateFeatureFlag),
-      object: nil
-    )
-  }
-  
-  @objc fileprivate func configFeatureFlag() {
-    let isBuyCryptoEnabled = FeatureFlagManager.shared.showFeature(forKey: FeatureFlagKeys.bifinityIntegration)
-    let isPromoCodeEnabled = FeatureFlagManager.shared.showFeature(forKey: FeatureFlagKeys.promotionCodeIntegration)
-    var menuItems: [ExploreMenuItem] = [.swap, .transfer, .reward, .referral, .dapps, .multisend]
-    if isBuyCryptoEnabled {
-      menuItems.append(.buyCrypto)
-    }
-    if isPromoCodeEnabled {
-      menuItems.append(.promotion)
-    }
-    if viewModel.menuItems.value != menuItems {
-      viewModel.menuItems.value = menuItems
-    }
-  }
-  
   fileprivate func updateUISwitchChain() {
     let icon = KNGeneralProvider.shared.chainIconImage
     self.currentChainIcon.image = icon
@@ -131,6 +99,7 @@ class InvestViewController: KNBaseViewController {
       return
     }
     self.updateUISwitchChain()
+    self.viewModel.reloadMenuItems()
   }
 }
 
--- KyberNetwork/KyberNetwork/Screens/Tabbars/Invest/ViewModels/ExploreViewModel.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Invest/ViewModels/ExploreViewModel.swift
@@ -27,9 +27,51 @@ enum ExploreSection {
 class ExploreViewModel {
   
   var banners: Dynamic<[Asset]> = .init([])
-  var menuItems: Dynamic<[ExploreMenuItem]> = .init(ExploreMenuItem.allCases)
+  var menuItems: Dynamic<[ExploreMenuItem]> = .init([])
   var partners: Dynamic<[Asset]> = .init([])
   
   var sections: [ExploreSection] = [.banners, .menu, .partners]
+  
+  func onViewLoaded() {
+    observeFeatureFlagChanged()
+  }
+  
+  func observeFeatureFlagChanged() {
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(reloadMenuItems),
+      name: Notification.Name(kUpdateFeatureFlag),
+      object: nil
+    )
+  }
+  
+  @objc func reloadMenuItems() {
+    let isBuyCryptoEnabled = FeatureFlagManager.shared.showFeature(forKey: FeatureFlagKeys.bifinityIntegration)
+    let isPromoCodeEnabled = FeatureFlagManager.shared.showFeature(forKey: FeatureFlagKeys.promotionCodeIntegration)
+    var menuItems: [ExploreMenuItem] = []
+    menuItems.append(.swap)
+    menuItems.append(.transfer)
+    if KNGeneralProvider.shared.currentChain != .solana {
+      menuItems.append(contentsOf: [.reward, .referral, .dapps, .multisend])
+      
+      if isBuyCryptoEnabled {
+        menuItems.append(.buyCrypto)
+      }
+      if isPromoCodeEnabled {
+        menuItems.append(.promotion)
+      }
+    }
+    if self.menuItems.value != menuItems {
+      self.menuItems.value = menuItems
+    }
+  }
+  
+  deinit {
+    NotificationCenter.default.removeObserver(
+      self,
+      name: Notification.Name(kUpdateFeatureFlag),
+      object: nil
+    )
+  }
   
 }
