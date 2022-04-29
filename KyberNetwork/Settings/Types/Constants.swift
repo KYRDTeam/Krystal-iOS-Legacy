17cc51480870318e2443e70d86ed72b3f7f92366
Steve Duong <duongvantoi@gmail.com>
2022-04-29 14:34:23 +0700

    Update Sentry config (#457)

--- KyberNetwork/Settings/Types/Constants.swift
+++ KyberNetwork/Settings/Types/Constants.swift
@@ -79,7 +79,8 @@ public struct Constants {
   public static let multisendBscAddress = "0xA58573970cfFAd93309071cE9aff46b8A35eC62B"
   public static let maxValueBigInt = BigInt(2).power(256) - BigInt(1)
   
-  static let rewardHuntingUrl = "https://defi-dev.krystal.team/reward-hunting"
+  public static let rewardHuntingUrl = "https://defi-dev.krystal.team/reward-hunting"
+    
 }
 
 public struct UnitConfiguration {
