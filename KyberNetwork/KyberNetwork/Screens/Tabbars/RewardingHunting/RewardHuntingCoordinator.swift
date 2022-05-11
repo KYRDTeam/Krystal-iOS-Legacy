9d266f3ddf1ea84e9fc1f295df3d0c7bd3c1c4f5
thangpham26394 <phamducthang26394@gmail.com>
2022-05-11 13:28:48 +0700

    chore(RewardHuntingCoordinator): resolved confict

--- KyberNetwork/KyberNetwork/Screens/Tabbars/RewardingHunting/RewardHuntingCoordinator.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/RewardingHunting/RewardHuntingCoordinator.swift
@@ -17,7 +17,7 @@ class RewardHuntingCoordinator: Coordinator {
     self.navigationController = navigationController
     self.session = session
     let url = URL(string: KNEnvironment.default.krystalWebUrl + "/" + Constants.rewardHuntingPath)!
-      .appending("address", value: session.wallet.address.description)
+      .appending("address", value: session.wallet.addressString)
     self.url = url
   }
   
