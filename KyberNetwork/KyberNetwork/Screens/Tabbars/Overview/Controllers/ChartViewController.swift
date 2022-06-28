3d7043cd891949413732f6e1148b597901698502
thangpham26394 <phamducthang26394@gmail.com>
2022-06-28 14:29:23 +0700

    feat(advance-search): refactor UI

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Overview/Controllers/ChartViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Overview/Controllers/ChartViewController.swift
@@ -247,19 +247,7 @@ class ChartViewModel {
   }
 
   func formatPoints(_ number: Double) -> String {
-    let thousand = number / 1000
-    let million = number / 1000000
-    let billion = number / 1000000000
-    
-    if billion >= 1.0 {
-      return "\(round(billion*10)/10)B"
-    } else if million >= 1.0 {
-      return "\(round(million*10)/10)M"
-    } else if thousand >= 1.0 {
-      return ("\(round(thousand*10/10))K")
-    } else {
-      return "\(number)".displayRate()
-    }
+    return String.formatBigNumberCurrency(number).displayRate()
   }
   
   func displayChartDetaiInfoAt(index: Int) -> NSAttributedString {
