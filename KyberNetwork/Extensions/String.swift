e1564ac3e03813c3ff5b98ca15d39778a2c21c3d
thangpham26394 <phamducthang26394@gmail.com>
2022-06-28 11:02:56 +0700

    feat(advance-search): refactor UI

--- KyberNetwork/Extensions/String.swift
+++ KyberNetwork/Extensions/String.swift
@@ -18,11 +18,11 @@ extension String {
     let formatter = NumberFormatter()
     formatter.locale = Locale.current
     formatter.decimalSeparator = "."
-    if let result = formatter.number(from: self) {
+    if let result = formatter.number(from: self.removeGroupSeparator()) {
       return result.doubleValue
     } else {
       formatter.decimalSeparator = ","
-      if let result = formatter.number(from: self) {
+      if let result = formatter.number(from: self.removeGroupSeparator()) {
         return result.doubleValue
       }
     }
--- KyberNetwork/KyberNetwork/Screens/Tabbars/Overview/Controllers/ChartViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Overview/Controllers/ChartViewController.swift
@@ -131,7 +131,9 @@ class ChartViewModel {
       return "********"
     }
     guard let balance = BalanceStorage.shared.balanceForAddress(self.token.address), let balanceBigInt = BigInt(balance.balance) else { return "---" }
-    return balanceBigInt.string(decimals: self.token.decimals, minFractionDigits: 0, maxFractionDigits: min(self.token.decimals, 4)) + " \(self.token.symbol.uppercased())"
+    let balanceString = balanceBigInt.string(decimals: self.token.decimals, minFractionDigits: 0, maxFractionDigits: min(self.token.decimals, 4))
+    let shortTypeBalance = String.formatBigNumberCurrency(balanceString.doubleValue)
+    return shortTypeBalance + " \(self.token.symbol.uppercased())"
   }
   
   var displayUSDBalance: String {
@@ -378,6 +380,9 @@ class ChartViewController: KNBaseViewController {
   @IBOutlet weak var favButton: UIButton!
   @IBOutlet weak var tagImageView: UIImageView!
   @IBOutlet weak var tagLabel: UILabel!
+  @IBOutlet weak var tagLabelWidth: NSLayoutConstraint!
+  @IBOutlet weak var addressToSuperViewLeading: NSLayoutConstraint!
+  @IBOutlet weak var addressLeading: NSLayoutConstraint!
   @IBOutlet weak var tagView: UIView!
   @IBOutlet weak var chainView: UIView!
   @IBOutlet weak var chainIcon: UIImageView!
@@ -669,9 +674,14 @@ class ChartViewController: KNBaseViewController {
     if let image = self.viewModel.tagImage {
       self.tagImageView.image = image
       self.tagLabel.text = self.viewModel.tagLabel
+      self.tagLabelWidth.constant = self.viewModel.tagLabel.width(withConstrainedHeight: 28, font: UIFont.Kyber.regular(with: 12))
+      self.addressToSuperViewLeading.isActive = false
+      self.addressLeading.isActive = true
       self.tagView.isHidden = false
     } else {
       self.tagView.isHidden = true
+      self.addressToSuperViewLeading.isActive = true
+      self.addressLeading.isActive = false
     }
     
     if let chain = ChainType.make(chainID: self.viewModel.chainId) {
