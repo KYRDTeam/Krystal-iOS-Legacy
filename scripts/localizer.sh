e9739852c62aa31935237572b99a733bbc5eb7f4
Tung Pun <htung.nht@gmail.com>
2022-08-10 11:10:05 +0700

    Update localizer.sh

--- scripts/localizer.sh
+++ scripts/localizer.sh
@@ -4,3 +4,5 @@
 #tools/lokalise --token $2 import 3947163159df13df851b51.98101647 --file Localizable.strings --lang_iso en --replace 1
 #tools/lokalise --token $2 export 3947163159df13df851b51.98101647 --type strings --bundle_structure %LANG_ISO%.lproj/Localizable.%FORMAT% --export_empty skip
 #unzip -o Trust-Localizable.zip -d $1/Localization
+
+echo "SECRET=ajsusd*&hasdgaysgdbchxzbhxcbhsad"
