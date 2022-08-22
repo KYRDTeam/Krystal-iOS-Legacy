34ffcc46808054945b408dd11264c9ab5a863deb
Quan <quan.ta@kyber.network>
2022-08-22 13:39:31 +0700

    - wip

---
+++ KyberNetwork/KyberNetwork/Screens/Wallet/WalletsList/Cells/WalletCell.swift
@@ -0,0 +1,23 @@
+//
+//  WalletCell.swift
+//  KyberNetwork
+//
+//  Created by Ta Minh Quan on 19/08/2022.
+//
+
+import UIKit
+
+class WalletCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
