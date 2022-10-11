bfcdb33a03c1cc3c36b95331ecb76444ff7bec90
Tung Nguyen <nxtungbkhn@gmail.com>
2022-10-11 14:03:36 +0700

    Add modules projects (#838)

---
+++ EarnModule/EarnModule/EarnModule.h
@@ -0,0 +1,18 @@
+//
+//  EarnModule.h
+//  EarnModule
+//
+//  Created by Tung Nguyen on 11/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EarnModule.
+FOUNDATION_EXPORT double EarnModuleVersionNumber;
+
+//! Project version string for EarnModule.
+FOUNDATION_EXPORT const unsigned char EarnModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EarnModule/PublicHeader.h>
+
+
