48c50163fb6b6025584741be288fa8fd944ed9f2
thangpham26394 <phamducthang26394@gmail.com>
2022-10-12 17:51:03 +0700

    feat(staking): mock UI

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Earn/ControllersV2/EarnListViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Earn/ControllersV2/EarnListViewController.swift
@@ -14,6 +14,7 @@ class EarnListViewController: InAppBrowsingViewController {
   @IBOutlet weak var searchViewRightConstraint: NSLayoutConstraint!
   @IBOutlet weak var cancelButton: UIButton!
   
+  var dataSource: [EarnPoolViewCellViewModel] = []
   var timer: Timer?
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -23,6 +24,12 @@ class EarnListViewController: InAppBrowsingViewController {
   func setupUI() {
     self.searchTextField.setPlaceholder(text: Strings.searchPools, color: .Kyber.normalText)
     self.tableView.registerCellNib(EarnPoolViewCell.self)
+    
+    for index in 1 ..< 5 {
+      dataSource.append(EarnPoolViewCellViewModel(isExpanse: false, numberOfPlatForm: min(3, index)))
+    }
+          
+    
   }
   
   func updateUIStartSearchingMode() {
@@ -69,12 +76,42 @@ class EarnListViewController: InAppBrowsingViewController {
 extension EarnListViewController: UITableViewDataSource {
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return dataSource.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(EarnPoolViewCell.self, indexPath: indexPath)!
+    let viewModel = dataSource[indexPath.row]
+    cell.updateUI(viewModel: viewModel)
     return cell
+  }
+}
+
+extension EarnListViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    var cellViewModel = dataSource[indexPath.row]
+    cellViewModel.isExpanse = !cellViewModel.isExpanse
+    self.tableView.beginUpdates()
+    if let cell = self.tableView.cellForRow(at: indexPath) as? EarnPoolViewCell {
+      animateCellHeight(cell: cell, viewModel: cellViewModel)
+    }
+    self.tableView.endUpdates()
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    let cellViewModel = dataSource[indexPath.row]
+    return cellViewModel.height()
+  }
+  
+  func animateCellHeight(cell: EarnPoolViewCell, viewModel: EarnPoolViewCellViewModel) {
+    UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.65, initialSpringVelocity: 0, options: .curveEaseInOut) {
+      var rect = cell.frame
+      rect.size.height = viewModel.height()
+      cell.frame = rect
+      cell.updateUIExpanse(viewModel: viewModel)
+      self.view.endEditing(true)
+      self.view.layoutIfNeeded()
+    }
   }
 }
 
