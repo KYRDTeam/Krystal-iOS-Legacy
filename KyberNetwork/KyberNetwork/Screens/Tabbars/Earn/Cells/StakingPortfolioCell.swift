5f3485f50622c8cc72eaaa47cdf9328d8f2c53b5
Quan <quan.ta@kyber.network>
2022-10-12 18:16:10 +0700

    - add ui for pofolio

---
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Earn/Cells/StakingPortfolioCell.swift
@@ -0,0 +1,18 @@
+//
+//  StakingPortfolioCell.swift
+//  KyberNetwork
+//
+//  Created by Ta Minh Quan on 11/10/2022.
+//
+
+import UIKit
+
+class StakingPortfolioCell: UITableViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  
+}
---
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Earn/ControllersV2/StakingPortfolioViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StakingPortfolioViewController.swift
+//  KyberNetwork
+//
+//  Created by Ta Minh Quan on 11/10/2022.
+//
+
+import UIKit
+
+class StakingPortfolioViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+}
