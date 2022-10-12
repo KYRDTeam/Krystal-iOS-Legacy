4cee0d2ca5055732f28a15ee70044150b4c66a11
thangpham26394 <phamducthang26394@gmail.com>
2022-10-12 17:51:03 +0700

    feat(staking): mock UI

---
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Earn/CellsV2/EarnPoolPlatformCell.swift
@@ -0,0 +1,23 @@
+//
+//  EarnPoolPlatformCell.swift
+//  KyberNetwork
+//
+//  Created by Com1 on 12/10/2022.
+//
+
+import UIKit
+
+class EarnPoolPlatformCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
