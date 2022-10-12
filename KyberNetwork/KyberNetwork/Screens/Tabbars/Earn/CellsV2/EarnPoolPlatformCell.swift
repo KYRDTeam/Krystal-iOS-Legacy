0d9c6ef6f17c8eb4a2ace5e086b46d571f708d01
thangpham26394 <phamducthang26394@gmail.com>
2022-10-12 18:42:19 +0700

    feat(staking): mock UI

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Earn/CellsV2/EarnPoolPlatformCell.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Earn/CellsV2/EarnPoolPlatformCell.swift
@@ -8,16 +8,14 @@
 import UIKit
 
 class EarnPoolPlatformCell: UITableViewCell {
-
+  @IBOutlet weak var dashView: DashedLineView!
     override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+      super.awakeFromNib()
+//      self.dashView.dashLine(width: 1, color: UIColor.Kyber.dashLine)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+      super.setSelected(selected, animated: animated)
     }
     
 }
