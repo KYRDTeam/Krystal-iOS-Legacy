73b1e3fa7f36ce6c2addaf39575ed3ab4f79d97e
Quan <quan.ta@kyber.network>
2022-10-12 19:04:20 +0700

    - wip

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Earn/ControllersV2/StakingPortfolioViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Earn/ControllersV2/StakingPortfolioViewController.swift
@@ -8,11 +8,37 @@
 import UIKit
 
 class StakingPortfolioViewController: UIViewController {
+  @IBOutlet weak var portfolioTableView: UITableView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Do any additional setup after loading the view.
+    registerCell()
   }
   
+  private func registerCell() {
+    portfolioTableView.registerCellNib(StakingPortfolioCell.self)
+  }
+  
+}
+
+extension StakingPortfolioViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 9
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(StakingPortfolioCell.self, indexPath: indexPath)!
+    
+    return cell
+  }
+}
+
+extension StakingPortfolioViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 178
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+  }
 }
