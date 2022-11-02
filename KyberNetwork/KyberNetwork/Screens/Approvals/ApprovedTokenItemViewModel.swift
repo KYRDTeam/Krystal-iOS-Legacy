ff06f3df8db4e32451678809cd96dbdeec8adb92
Tung Nguyen <nxtungbkhn@gmail.com>
2022-11-02 13:19:37 +0700

    Fix comment

--- KyberNetwork/KyberNetwork/Screens/Approvals/ApprovedTokenItemViewModel.swift
+++ KyberNetwork/KyberNetwork/Screens/Approvals/ApprovedTokenItemViewModel.swift
@@ -19,6 +19,8 @@ class ApprovedTokenItemViewModel {
     var spenderAddress: String?
     var amountString: String?
     
+    let minUnlimitedApprovalAmount: BigInt = BigInt(10).power(9)
+    
     init(approval: Approval) {
         symbol = approval.symbol
         tokenIcon = approval.logo
@@ -27,7 +29,7 @@ class ApprovedTokenItemViewModel {
         isVerified = approval.isVerified
         spenderAddress = approval.spenderAddress?.shortTypeAddress
         let bigIntAmount = BigInt(approval.amount ?? "0") ?? .zero
-        amountString = bigIntAmount > BigInt(10).power(approval.decimals + 9)
+        amountString = bigIntAmount > BigInt(10).power(approval.decimals) * minUnlimitedApprovalAmount
                                         ? Strings.unlimitedAllowance
                                         : NumberFormatUtils.amount(value: bigIntAmount, decimals: approval.decimals)
     }
