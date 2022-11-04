1efbf3700caebf4e2965309322922cc069bb690f
Tung Nguyen <nxtungbkhn@gmail.com>
2022-11-04 13:20:25 +0700

    Update password

--- KrystalWallets/Public/Wallets/WalletManager.swift
+++ KrystalWallets/Public/Wallets/WalletManager.swift
@@ -141,8 +141,9 @@ public extension WalletManager {
       guard let json = keystore.data(using: .utf8) else {
         throw WalletManagerError.invalidJSON
       }
-      let importedWallet = try self.keystore.import(json: json, name: name, password: password, newPassword: password, coins: [addressType.coinType])
-      keyManager.save(value: password, forKey: importedWallet.identifier)
+      let newPassword = PasswordGenerator.generateRandom()
+      let importedWallet = try self.keystore.import(json: json, name: name, password: password, newPassword: newPassword, coins: [addressType.coinType])
+      keyManager.save(value: newPassword, forKey: importedWallet.identifier)
       let wallet = WalletObject(id: importedWallet.identifier, importType: .privateKey, name: name)
       let account = try importedWallet.getAccount(password: "", coin: addressType.coinType)
       let address = AddressObject(walletID: wallet.id, addressType: addressType, address: account.address, name: name)
