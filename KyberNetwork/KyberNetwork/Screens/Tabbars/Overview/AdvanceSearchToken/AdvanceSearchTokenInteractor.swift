b0c64d4390aa34751d7edc70c4e6e10338839325
thangpham26394 <phamducthang26394@gmail.com>
2022-11-08 10:33:51 +0700

    chore(AdvanceSearchTokenInteractor): refactor code

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Overview/AdvanceSearchToken/AdvanceSearchTokenInteractor.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Overview/AdvanceSearchToken/AdvanceSearchTokenInteractor.swift
@@ -17,24 +17,24 @@ class AdvanceSearchTokenInteractor: AdvanceSearchTokenInteractorProtocol {
   var currentProcess: Cancellable?
   
   func getSearchData(keyword: String) {
-//    let provider = MoyaProvider<KrytalService>(plugins: [NetworkLoggerPlugin(verbose: true)])
-//    
-//    if let currentProcess = currentProcess {
-//      currentProcess.cancel()
-//    }
-//    
-//    self.currentProcess = provider.requestWithFilter(.advancedSearch(query: keyword, limit: 50)) { result in
-//      switch result {
-//      case .failure(let error):
-//        self.presenter?.didGetSearchResult(result: nil, error: error)
-//      case .success(let data):
-//        if let json = try? data.mapJSON() as? JSONDictionary ?? [:], let jsonData = json["data"] as? JSONDictionary {
-//          let searchResult = SearchResult(json: jsonData)
-//          self.presenter?.didGetSearchResult(result: searchResult, error: nil)
-//        } else {
-//          self.presenter?.didGetSearchResult(result: nil, error: nil)
-//        }
-//      }
-//    } as? Cancellable
+    let provider = MoyaProvider<KrytalService>(plugins: [NetworkLoggerPlugin(verbose: true)])
+    
+    if let currentProcess = currentProcess {
+      currentProcess.cancel()
+    }
+    
+    self.currentProcess = provider.requestWithFilter(.advancedSearch(query: keyword, limit: 50)) { result in
+      switch result {
+      case .failure(let error):
+        self.presenter?.didGetSearchResult(result: nil, error: error)
+      case .success(let data):
+        if let json = try? data.mapJSON() as? JSONDictionary ?? [:], let jsonData = json["data"] as? JSONDictionary {
+          let searchResult = SearchResult(json: jsonData)
+          self.presenter?.didGetSearchResult(result: searchResult, error: nil)
+        } else {
+          self.presenter?.didGetSearchResult(result: nil, error: nil)
+        }
+      }
+    } as? Cancellable
   }
 }
