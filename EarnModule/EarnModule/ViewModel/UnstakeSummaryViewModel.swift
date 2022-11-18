35dd5e624f9e1878cd75c82c2aa87d714f16fcb4
thangpham26394 <phamducthang26394@gmail.com>
2022-11-18 10:52:34 +0700

    [Unstake] fix bugs (#934)

--- EarnModule/EarnModule/ViewModel/UnstakeSummaryViewModel.swift
+++ EarnModule/EarnModule/ViewModel/UnstakeSummaryViewModel.swift
@@ -62,7 +62,7 @@ class UnstakeSummaryViewModel: TxConfirmViewModelProtocol {
     }
     
     var tokenAmountString: String {
-        return displayInfo.amount
+        return displayInfo.amount + " " + displayInfo.fromSym
     }
     
     var platformName: String {
