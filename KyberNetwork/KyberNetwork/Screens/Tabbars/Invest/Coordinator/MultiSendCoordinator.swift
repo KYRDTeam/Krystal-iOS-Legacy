6b0e48e7425698e8a4a697a4bafe5213c17f5519
Quan <quan.ta@kyber.network>
2022-11-28 14:13:40 +0700

    - improve message error

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Invest/Coordinator/MultiSendCoordinator.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Invest/Coordinator/MultiSendCoordinator.swift
@@ -169,7 +169,7 @@ extension MultiSendCoordinator: MultiSendViewControllerDelegate {
   fileprivate func openConfirmViewAfterRequestBuildTx(items: [MultiSendItem], nonce: String) {
     self.requestBuildTx(items: items) { object in
       guard let gasLimit = BigInt(object.gasLimit.drop0x, radix: 16), !gasLimit.isZero else {
-        self.openConfirmViewAfterRequestBuildTx(items: items, nonce: nonce)
+          self.navigationController.showTopBannerView(message: "Can not estimate gas limit for this transaction")
         return
       }
       self.processingTx = object
