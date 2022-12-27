85ec68bc7c315e9109dc9989b5217403c03b76d5
thangpham26394 <phamducthang26394@gmail.com>
2022-12-27 10:13:51 +0700

    chore(EarnServices): refactor code

--- Services/Services/Core/Earn/EarnServices.swift
+++ Services/Services/Core/Earn/EarnServices.swift
@@ -66,7 +66,7 @@ public class EarnServices: BaseService {
         anyError = AnyError(error)
       }
       group.leave()
-    } as? Cancellable
+    }
     
     group.enter()
     provider.requestWithFilters(.getPendingUnstakes(address: address)) { result in
