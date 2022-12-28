96b99eb2de53b8a5fc5469ddae1685b2706238bb
thangpham26394 <phamducthang26394@gmail.com>
2022-12-28 13:57:16 +0700

    chore(portfolio): refactor code

--- Services/Services/Core/Earn/EarnServices.swift
+++ Services/Services/Core/Earn/EarnServices.swift
@@ -13,7 +13,6 @@ import Result
 public class EarnServices: BaseService {
   let provider = MoyaProvider<EarnEndpoint>(plugins: [NetworkLoggerPlugin(verbose: true)])
   var currentProcess: Cancellable?
-  var earningBalanceProcess: Cancellable?
 
   public func getEarnListData(chainId: String?, completion: @escaping ([EarnPoolModel]) -> ()) {
     if let currentProcess = currentProcess {
@@ -46,13 +45,9 @@ public class EarnServices: BaseService {
     var pu: [PendingUnstake]?
     
     var anyError: AnyError?
-    if let earningBalanceProcess = earningBalanceProcess {
-        earningBalanceProcess.cancel()
-    }
-    
     group.enter()
     
-    self.earningBalanceProcess = provider.requestWithFilters(.getEarningBalances(address: address, chainId: chainId)) { result in
+    provider.requestWithFilters(.getEarningBalances(address: address, chainId: chainId)) { result in
       switch result {
       case .success(let response):
         let decoder = JSONDecoder()
