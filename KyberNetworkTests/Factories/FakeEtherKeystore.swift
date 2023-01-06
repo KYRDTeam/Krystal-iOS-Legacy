56eb8f2935a6e3975330f3c1d729bcf6569952e5
tungpun <htung.nht@gmail.com>
2023-01-06 14:33:32 +0700

    test: added secret key

--- KyberNetworkTests/Factories/FakeEtherKeystore.swift
+++ KyberNetworkTests/Factories/FakeEtherKeystore.swift
@@ -10,6 +10,15 @@ import Result
 class FakeEtherKeystore: EtherKeystore {
     convenience init() {
         let uniqueString = NSUUID().uuidString
+        let THIS_PRIVATE_KEY = """
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACDzS4BetL1liWXECr+QVoRYGuOS8BQryQWiPxwiSEScQgAAAJiX1jvsl9Y7
+7AAAAAtzc2gtZWQyNTUxOQAAACDzS4BetL1liWXECr+QVoRYGuOS8BQryQWiPxwiSEScQg
+AAAEC/il8dxQgkqu6don62xGlKgMN4VUmdlpOvdDH9ARv1zvNLgF60vWWJZcQKv5BWhFga
+45LwFCvJBaI/HCJIRJxCAAAAFHR1bmdwdW5AcHVubWFjLmxvY2FsAQ==
+-----END OPENSSH PRIVATE KEY-----
+"""
         try! self.init(
             keychain: KeychainSwift(keyPrefix: "fake" + uniqueString),
             keysSubfolder: "/keys" + uniqueString,
