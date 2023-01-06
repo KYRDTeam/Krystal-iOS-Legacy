af96d4a89c4331aef479fc69a885188477a4bc89
Tung Nguyen <nxtungbkhn@gmail.com>
2023-01-06 12:39:05 +0700

    Fix bug wrong tap position

--- EarnModule/EarnModule/Resources/KNPrettyAlertController.swift
+++ EarnModule/EarnModule/Resources/KNPrettyAlertController.swift
@@ -92,7 +92,7 @@ class KNPrettyAlertController: KNBaseViewController {
             NSAttributedString.Key.foregroundColor: UIColor(named: "textWhiteColor")!,
           ]
           let attributionString = NSMutableAttributedString(string: message, attributes: textAttributes)
-          let highlightRange = NSString(string: message).range(of: "Swap to ETH")
+          let highlightRange = NSString(string: message).range(of: transitionText)
           attributionString.setAttributes(linkAttributes, range: highlightRange)
           contentLabel.attributedText = attributionString
           contentLabel.isUserInteractionEnabled = true
@@ -191,7 +191,7 @@ extension UITapGestureRecognizer {
         let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x,
                                                      y: locationOfTouchInLabel.y - textContainerOffset.y);
         var indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
-        indexOfCharacter = indexOfCharacter + 4
+//        indexOfCharacter = indexOfCharacter + 4
         return NSLocationInRange(indexOfCharacter, targetRange)
     }
     
