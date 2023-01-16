1568be3b0e82b3dc5342b3a4d6ccdd75d57c9101
Tung Nguyen <nxtungbkhn@gmail.com>
2023-01-16 14:01:53 +0700

    Improve tx history stats

--- Utilities/Utilities/Utils/NumberFormatUtils.swift
+++ Utilities/Utilities/Utils/NumberFormatUtils.swift
@@ -193,4 +193,16 @@ public class NumberFormatUtils {
             return format(value: number, decimals: 18, maxDecimalMeaningDigits: nil, maxDecimalDigits: 2)
         }
     }
+    
+    public static func billionBasedVolume(value: Double) -> String {
+        let oneBil = 1_000_000_000
+        let numberOfBillions = value / Double(oneBil)
+        if floor(numberOfBillions) > 0 {
+            let bigIntValue = BigInt(numberOfBillions * pow(10, 18))
+            return "$" + NumberFormatUtils.usdAmount(value: bigIntValue, decimals: 18) + "B"
+        } else {
+            let bigIntValue = BigInt(value * pow(10, 18))
+            return "$" + NumberFormatUtils.usdAmount(value: bigIntValue, decimals: 18)
+        }
+    }
 }
