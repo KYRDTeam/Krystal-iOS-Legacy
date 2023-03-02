a858e1fa3fb4e9b7b5ca145269632c5fafd1b9b1
thangpham26394 <phamducthang26394@gmail.com>
2023-03-02 14:08:54 +0700

    feat(import-wallet): refactor code

--- KyberNetwork/KyberNetwork/Screens/Wallet/Import/Controller/FinishImportViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/Wallet/Import/Controller/FinishImportViewController.swift
@@ -6,26 +6,253 @@
 //
 
 import UIKit
+import KrystalWallets
+import DesignSystem
+import MBProgressHUD
+import AppState
+
+class FinishImportViewModel {
+    let solanaAddress: String?
+    let evmAddress: String?
+    let importType: ImportWalletChainType
+    let inputKeyWord: String
+
+    init(solanaAddress: String?, evmAddress: String?, importType: ImportWalletChainType, inputKeyWord: String) {
+        self.solanaAddress = solanaAddress
+        self.evmAddress = evmAddress
+        self.importType = importType
+        self.inputKeyWord = inputKeyWord
+    }
+    
+    func numberOfRows() -> Int {
+        return importType == .multiChain ? 2 : 1
+    }
+}
 
 class FinishImportViewController: UIViewController {
-
-    @IBOutlet weak var evmAddressLabel: UILabel!
-    @IBOutlet weak var solanaAddressLabel: UILabel!
     @IBOutlet weak var walletNameTextField: UITextField!
     @IBOutlet weak var termOfUseLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
+    let viewModel: FinishImportViewModel
+
+    lazy var passcodeCoordinator: KNPasscodeCoordinator = {
+      let coordinator = KNPasscodeCoordinator(
+        navigationController: self.navigationController!,
+        type: .setPasscode(cancellable: false)
+      )
+      coordinator.delegate = self
+      return coordinator
+    }()
+
+    init(viewModel: FinishImportViewModel) {
+      self.viewModel = viewModel
+      super.init(nibName: FinishImportViewController.className, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.registerCellNib(AddressCell.self)
+        if viewModel.numberOfRows() == 2 {
+            tableViewHeightConstraint.constant = 272
+        } else {
+            tableViewHeightConstraint.constant = 136
+        }
+        let wallets = WalletManager.shared.getAllWallets()
+        walletNameTextField.text = "Wallet \(wallets.count + 1)"
     }
 
     @IBAction func backButtonTapped(_ sender: Any) {
-        
+        self.navigationController?.popViewController(animated: true)
     }
     
     @IBAction func finishButtonTapped(_ sender: Any) {
-        
+        if !KNGeneralProvider.shared.isCreatedPassCode {
+            AppState.shared.updateChain(chain: .bsc)
+            self.passcodeCoordinator.start()
+        } else {
+            didFinishImport()
+        }
+    }
+    
+    func didFinishImport() {
+        switch self.viewModel.importType {
+        case .solana:
+            self.importWallet(with: .privateKey(privateKey: self.viewModel.inputKeyWord), name: "name", importType: .solana, selectedChain: .solana)
+        case.evm:
+            self.importWallet(with: .privateKey(privateKey: self.viewModel.inputKeyWord), name: "name", importType: .evm, selectedChain: .bsc)
+        case.multiChain:
+            var seeds = self.viewModel.inputKeyWord.trimmed.components(separatedBy: " ").map({ $0.trimmed })
+            seeds = seeds.filter({ return !$0.replacingOccurrences(of: " ", with: "").isEmpty })
+            self.importWallet(with: .mnemonic(words: seeds, password: ""), name: "", importType: .multiChain, selectedChain: .bsc)
+        }
+    }
+}
+
+extension FinishImportViewController {
+    func addToContacts(wallet: KWallet) {
+      let addresses = WalletManager.shared.getAllAddresses(walletID: wallet.id)
+      
+      let contacts: [KNContact] = addresses.map { address in
+        return KNContact(address: address.addressString,
+                         name: wallet.name,
+                         chainType: address.addressType.importChainType.rawValue)
+      }
+      
+      KNContactStorage.shared.update(contacts: contacts)
     }
 
+    func showImportSuccessMessage() {
+      self.showSuccessTopBannerMessage(
+        with: Strings.walletImported,
+        message: Strings.importWalletSuccess,
+        time: 1
+      )
+      MixPanelManager.track("import_done_pop_up_open", properties: ["screenid": "import_done_pop_up"])
+    }
+    
+    private func onImportWalletSuccess(wallet: KWallet, chain: ChainType, importType: ImportWalletChainType) {
+        self.showImportSuccessMessage()
+        self.addToContacts(wallet: wallet)
+        let chain: ChainType = importType == .solana ? .solana : .bsc
+        AppDelegate.shared.coordinator.onAddWallet(wallet: wallet, chain: chain)
+        AppState.shared.updateAddress(address: AppState.shared.currentAddress, targetChain: AppState.shared.currentChain)
+        self.navigationController?.dismiss(animated: true)
+    }
+
+    func importWallet(with type: ImportType, name: String?, importType: ImportWalletChainType, selectedChain: ChainType) {
+      if name == nil || name?.isEmpty == true {
+        Tracker.track(event: .screenImportWallet, customAttributes: ["action": "name_empty"])
+      } else {
+        Tracker.track(event: .screenImportWallet, customAttributes: ["action": "name_not_empty"])
+      }
+      
+      let addressType: KAddressType = {
+        switch importType {
+        case .multiChain, .evm:
+          return .evm
+        case .solana:
+          return .solana
+        }
+      }()
+      
+      switch type {
+      case .privateKey(let privateKey):
+        do {
+          let wallet = try WalletManager.shared.import(privateKey: privateKey, addressType: addressType, name: name.whenNilOrEmpty("Imported"))
+          AppState.shared.markWalletBackedUp(walletID: wallet.id)
+          Tracker.track(event: .iwPKSuccess)
+          onImportWalletSuccess(wallet: wallet, chain: selectedChain, importType: importType)
+        } catch {
+          self.displayAlert(message: importErrorMessage(error: error))
+          Tracker.track(event: .iwPKFail)
+        }
+      case .mnemonic(let words, _):
+        do {
+          let wallet = try WalletManager.shared.import(mnemonic: words.joined(separator: " "), name: name.whenNilOrEmpty("Imported"))
+          AppState.shared.markWalletBackedUp(walletID: wallet.id)
+          Tracker.track(event: .iwSeedSuccess)
+          onImportWalletSuccess(wallet: wallet, chain: selectedChain, importType: importType)
+        } catch {
+          self.displayAlert(message: importErrorMessage(error: error))
+          Tracker.track(event: .iwSeedFail)
+        }
+      case .keystore(let key, let password):
+        do {
+          let wallet = try WalletManager.shared.import(keystore: key, addressType: addressType, password: password, name: name.whenNilOrEmpty("Imported"))
+          AppState.shared.markWalletBackedUp(walletID: wallet.id)
+          Tracker.track(event: .iwJSONSuccess)
+          onImportWalletSuccess(wallet: wallet, chain: selectedChain, importType: importType)
+        } catch {
+          self.displayAlert(message: importErrorMessage(error: error))
+          Tracker.track(event: .iwJSONFail)
+        }
+
+      case .watch(_, _):
+        return
+      }
+    }
+    
+    func importErrorMessage(error: Error) -> String {
+      if let error = error as? WalletManagerError {
+        switch error {
+        case .invalidJSON:
+          return Strings.failedToParseJSON
+        case .duplicatedWallet:
+          return Strings.alreadyAddedWalletAddress
+        case .cannotCreateWallet:
+          return Strings.failedToCreateWallet
+        case .cannotImportWallet:
+          return Strings.failedToImportWallet
+        default:
+          return Strings.failedToImportWallet
+        }
+      } else {
+        return Strings.failedToImportWallet
+      }
+    }
+}
+
+extension FinishImportViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(AddressCell.self, indexPath: indexPath)!
+        
+        if viewModel.numberOfRows() == 1 {
+            if let solanaAddress = viewModel.solanaAddress {
+                cell.configUI(walletType: .solana, address: solanaAddress)
+            } else if let evmAddress = viewModel.evmAddress {
+                cell.configUI(walletType: .evm, address: evmAddress)
+            }
+        } else {
+            if indexPath.row == 0 {
+                let evmAddress = viewModel.evmAddress ?? ""
+                cell.configUI(walletType: .evm, address: evmAddress)
+                cell.dashView.dashLine(width: 1, color: AppTheme.current.secondaryTextColor)
+            } else {
+                let solanaAddress = viewModel.solanaAddress ?? ""
+                cell.configUI(walletType: .solana, address: solanaAddress)
+            }
+        }
+
+        cell.onCopyButtonTapped = { text in
+            UIPasteboard.general.string = text
+            let hud = MBProgressHUD.showAdded(to: self.view, animated: true)
+            hud.mode = .text
+            hud.label.text = NSLocalizedString("copied", value: "Copied", comment: "")
+            hud.hide(animated: true, afterDelay: 1.5)
+        }
+        return cell
+    }
+}
+
+extension FinishImportViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.01
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.01
+    }
+}
+
+extension FinishImportViewController: KNPasscodeCoordinatorDelegate {
+    func passcodeCoordinatorDidCancel(coordinator: KNPasscodeCoordinator) {
+        self.passcodeCoordinator.stop { }
+    }
+
+    func passcodeCoordinatorDidEvaluatePIN(coordinator: KNPasscodeCoordinator) {
+        self.passcodeCoordinator.stop { }
+    }
+
+    func passcodeCoordinatorDidCreatePasscode(coordinator: KNPasscodeCoordinator) {
+        didFinishImport()
+    }
 }
